14eda38669f9f5532ab51dcffdea2c18fdaea5a4
unknown <telmo.latas@netcabo.pt>
2018-03-26 21:17:26 +0100

    inicio

---
+++ src/OlaMudo.java
@@ -0,0 +1,8 @@
+
+public class OlaMudo {
+
+	public static void main(String[] args) {
+		System.out.println(" Olá Mundo");
+	}
+
+}
---
+++ src/teste/CTeste.java
@@ -0,0 +1,10 @@
+package teste;
+
+public class CTeste {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo");
+
+	}
+
+}
